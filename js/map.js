2ad8008b64d518aa42a64a9599950b3d39ee1fad
Anton <tuzhikaa@gmail.com>
2017-07-21 20:32:27 +0500

    vstart

---
+++ js/map.js
@@ -0,0 +1,2 @@
+
+ymaps.ready(init);var myMap,myPlacemark;function init(){myMap=new ymaps.Map("map",{center:[59.936574,30.321619],zoom:16,controls:["zoomControl"]});myPlacemark=new ymaps.Placemark([59.936574,30.321619],{hintContent:'Мишка',balloonContent:'Милые штучки ручной работы для дома',iconLayout:'default#image',iconImageHref:'/build/img/icon-map-pin.svg',iconImageSize:[66,100],iconImageOffset:[-3,-42]});myMap.geoObjects.add(myPlacemark);}
